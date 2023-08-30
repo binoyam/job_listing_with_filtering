f91244c86aaa79eec1b03e147f108fc555e77757
binoyam <btechan@gmail.com>
2023-08-31 02:27:12 +0300

    mapped

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import './App.css'
+import Header from './Header'
+
+import JobListing from './JobListing'
+
+function App() {
+
+  return (
+    <div className='App'>
+      <Header />
+      <div className="main">
+      <JobListing />
+      </div>
+    </div>
+
+   
+  )
+}
+
+export default App
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import headerImg from './images/bg-header-desktop.svg';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <img src={headerImg} alt="" />
+        </div>
+    );
+}
+
+export default Header;
