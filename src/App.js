b5fe981ab89a1dfafb958971f327d5d913b152dd
binoyam <binyam.techan01@gmail.com>
2023-09-21 02:44:08 +0300

    starter

--- src/App.js
+++ src/App.js
@@ -1,42 +1,25 @@
-import data from './data.json'
-import Jobs from './components/Jobs'
-import { useState } from 'react'
-import Header from './components/Header'
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [filterKeywords, setFilterKeywords] = useState([])
-
-  const addFilterKeywords = (data) => {
-    if (!filterKeywords.includes(data)) {
-      setFilterKeywords([...filterKeywords, data]);
-    }
-  };
-
-  const deleteKeyword = (data) => {
-    const newKeywords = filterKeywords.filter((key) => key !== data);
-    setFilterKeywords(newKeywords);
-  };
-
-  const clearAll = () => {
-    setFilterKeywords([]);
-  };
-
   return (
-    <div>
-      <div className="header"></div>
-      {filterKeywords.length > 0 && <Header keywords={filterKeywords}
-      removeKeywords={deleteKeyword}
-      clearAll={clearAll}
-      />}
-      <Jobs data={data}
-       keywords={filterKeywords}
-       setKeywords={addFilterKeywords}
-      />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-
-
-  )
+  );
 }
 
-export default App
+export default App;
