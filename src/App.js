e8952df3517f03b33835190a225c4b66cff4c17d
binoyam <binyam.techan01@gmail.com>
2023-09-22 04:21:01 +0300

    header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="jobs-container-div">
+        <div className="job">
+          <div className="job-logo">
+            <img src="./images/photosnap.svg" alt="" />
+          </div>
+          <div className="job-info-div">
+            <h4>
+              Photosnap
+              <span>New!</span>
+              <span>Featured</span>
+            </h4>
+            <h3>Senior Frontend Developer</h3>
+            <p>
+              <span> 1d ago</span>
+              <span> Full Time</span>
+              <span> USA only</span>
+            </p>
+          </div>
+          <div className="skills-div">
+            <span> Frontend</span>
+            <span> Senior</span>
+            <span> HTML</span>
+            <span>CSS</span>
+            <span>JavaScript</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import HeaderImg from '../images/bg-header-desktop.svg'
+function Header() {
+  return (
+    <header>
+        <img src={HeaderImg} alt="header image" />
+    </header>
+  )
+}
+
+export default Header
