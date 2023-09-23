ef0c3242fa5f06221df2e5fff93678c254b542cd
binoyam <binyam.techan01@gmail.com>
2023-09-24 02:16:19 +0300

    desktop version done

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,26 +5,29 @@ import closeBtn from "./images/icon-remove.svg";
 function Header({ keyWords, deleteKeyWords, clearAll }) {
   return (
     <header>
-      <img src={HeaderImg} alt="header image" />
-      <div className="filter-bar">
-        <ul>
-          {keyWords.map((key, id) => {
-            return (
-              <li key={id}>
-                {key}
-                <button 
-                onClick={() => deleteKeyWords(key)}
-                className="delete-btn">
-                  <img src={closeBtn} alt="close button" />
-                </button>
-              </li>
-            );
-          })}
-          <a className="clear-btn" href="/#" onClick={() => clearAll()}>
-            Clear
-          </a>
-        </ul>
-      </div>
+      <img src={HeaderImg} alt="header-background" />
+      {keyWords.length > 0 && (
+        <div className="filter-bar">
+          <ul>
+            {keyWords.map((key, id) => {
+              return (
+                <li key={id}>
+                  <span className="key">{key}</span>
+                  <button
+                    onClick={() => deleteKeyWords(key)}
+                    className="delete-btn"
+                  >
+                    <img src={closeBtn} alt="close button" />
+                  </button>
+                </li>
+              );
+            })}
+            <a className="clear-btn" href="/#" onClick={() => clearAll()}>
+              Clear
+            </a>
+          </ul>
+        </div>
+      )}
     </header>
   );
 }
